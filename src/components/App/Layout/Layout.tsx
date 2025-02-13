d8d15f9934bccd2d9a9b948cf5fea54c2c632354
Mario <mario@mariogl.com>
2025-02-13 08:40:35 +0100

    feat(nav): add navigation menu

--- src/components/App/Layout/Layout.tsx
+++ src/components/App/Layout/Layout.tsx
@@ -3,12 +3,14 @@ import "./Layout.css";
 import Protected from "../../../auth/components/Protected/Protected";
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
+import Navigation from "../../Navigation/Navigation";
 
 const Layout: React.FC = () => {
   return (
     <>
       <div className="container">
         <Header />
+        <Navigation />
         <div className="main-block">
           <main className="main-content">
             <Protected>
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router";
+import "./Navigation.css";
+import { useAuthContext } from "../../auth/AuthContext/useAuthContext";
+
+const Navigation: React.FC = () => {
+  const challengeNumbers = [1, 2, 3, 4] as const;
+  const { userMaxChallenge } = useAuthContext();
+
+  return (
+    <nav className="main-navigation">
+      <ul>
+        {challengeNumbers.map((challengeNumber) => (
+          <li
+            key={challengeNumber}
+            className={`main-navigation__number${
+              challengeNumber < userMaxChallenge
+                ? " main-navigation__number--passed"
+                : ""
+            }`}
+          >
+            <Link to={`/deliveries/challenge-${challengeNumber}`}>
+              {challengeNumber}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;

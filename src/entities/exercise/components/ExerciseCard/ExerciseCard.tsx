4a5ecf5022c11810939c82a528f98d90159be622
Mario <mario@mariogl.com>
2025-02-17 13:28:59 +0100

    feat(deliveries): hide delivery button on non current challenges

--- src/entities/exercise/components/ExerciseCard/ExerciseCard.tsx
+++ src/entities/exercise/components/ExerciseCard/ExerciseCard.tsx
@@ -5,6 +5,7 @@ import { useDeliveriesQuery } from "../../../delivery/queries/useDeliveriesQuery
 import DeliveriesList from "../../../delivery/components/DeliveriesList/DeliveriesList";
 import "./ExerciseCard.css";
 import { useSearchParams } from "react-router";
+import { useAuthContext } from "../../../../auth/AuthContext/useAuthContext";
 
 interface ExerciseProps {
   exercise: Exercise;
@@ -12,7 +13,7 @@ interface ExerciseProps {
 
 const ExerciseCard: React.FC<ExerciseProps> = ({ exercise }) => {
   const [isFormOpen, setIsFormOpen] = useState(false);
-
+  const { userMaxChallenge, role } = useAuthContext();
   const [searchParams] = useSearchParams();
 
   const student = searchParams.get("student");
@@ -35,9 +36,11 @@ const ExerciseCard: React.FC<ExerciseProps> = ({ exercise }) => {
     <div className="exercise">
       <header className="exercise__header">
         <h4 className="exercise__name">{exercise.name}</h4>
-        <button className="button" onClick={toggleFormOpen}>
-          Entregar
-        </button>
+        {(exercise.challenge === userMaxChallenge || role === "admin") && (
+          <button className="button" onClick={toggleFormOpen}>
+            Entregar
+          </button>
+        )}
       </header>
       <p className="exercise__comments">{exercise.comments}</p>
       {deliveries && <DeliveriesList deliveries={deliveries} />}
